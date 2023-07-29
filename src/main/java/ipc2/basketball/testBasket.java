85b7d1a1c61c98faf2ef1a345b65be189b4475b4
Jbrod5 <jorgebravo202131782@cunoc.edu.gt>
2023-07-29 01:58:26 -0600

    Juego Basket

---
+++ src/main/java/ipc2/basketball/testBasket.java
@@ -0,0 +1,13 @@
+
+package ipc2.basketball;
+
+/**
+ *
+ * @author jorge
+ */
+public class testBasket {
+    public static void main(String[] args) {
+        Basketball basketball = new Basketball();
+        basketball.Juego();
+    }
+}
