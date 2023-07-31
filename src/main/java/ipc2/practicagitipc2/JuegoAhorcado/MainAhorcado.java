1b69af6fee8e38111ce6ecb69469e03686217ae7
Sergio Rodriguez <sergiorodriguez202131443@cunoc.edu.gt>
2023-07-31 00:29:16 -0600

    Subiendo juego de ahorcado

---
+++ src/main/java/ipc2/practicagitipc2/JuegoAhorcado/MainAhorcado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ipc2.practicagitipc2.JuegoAhorcado;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MainAhorcado {
+    public static void main(String[] args) {
+        JugarAhorcado juego = new JugarAhorcado();
+        juego.IniciarJuego();
+    }
+}
